8849d3308dc18b78b4ce321554fdf653309da542
Bibo Hao <haobibo@users.noreply.github.com>
2024-06-28 02:33:39 +0800

    allow baseUrl for one-api

--- router/web.go
+++ router/web.go
@@ -14,13 +14,13 @@ import (
 	"strings"
 )
 
-func SetWebRouter(router *gin.Engine, buildFS embed.FS) {
+func SetWebRouter(engine *gin.Engine, router *gin.RouterGroup, buildFS embed.FS) {
 	indexPageData, _ := buildFS.ReadFile(fmt.Sprintf("web/build/%s/index.html", config.Theme))
 	router.Use(gzip.Gzip(gzip.DefaultCompression))
 	router.Use(middleware.GlobalWebRateLimit())
 	router.Use(middleware.Cache())
 	router.Use(static.Serve("/", common.EmbedFolder(buildFS, fmt.Sprintf("web/build/%s", config.Theme))))
-	router.NoRoute(func(c *gin.Context) {
+	engine.NoRoute(func(c *gin.Context) {
 		if strings.HasPrefix(c.Request.RequestURI, "/v1") || strings.HasPrefix(c.Request.RequestURI, "/api") {
 			controller.RelayNotFound(c)
 			return
